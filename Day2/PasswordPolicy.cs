3c130e44a88ccd6d4ed6b3f9351a65524b4bfc36
Tony Lopar <tonylopar@hotmail.com>
2020-12-02 21:32:57 +0100

    Added results of part 2

---
+++ Day2/PasswordPolicy.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+
+namespace Day2
+{
+    class PasswordPolicy
+    {
+        int FirstNumber { get; }
+
+        int SecondNumber { get; }
+
+        char Character { get; }
+
+        string Password { get; }
+
+        public PasswordPolicy(int min, int max, char character, string password)
+        {
+            SecondNumber = max;
+            FirstNumber = min;
+            Character = character;
+            Password = password;
+        }
+
+        public bool SatisfiesLengthConstraint()
+        {
+            var numberOfChar = Password.ToCharArray().Count(x => x == Character);
+            return numberOfChar >= FirstNumber && numberOfChar <= SecondNumber;
+        }
+
+        public bool SatisfiesPositionConstraint()
+        {
+            return Password[FirstNumber - 1] == Character ^ Password[SecondNumber - 1] == Character;
+        }
+    }
+}
---
+++ Day2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var correctPasswordsPart1 = 0;
+            var correctPasswordsPart2 = 0;
+
+            var path = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, args[0]));
+            var file = new StringReader(path);
+
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                var min = line.Substring(0, line.IndexOf("-"));
+                var max = line.Substring(line.IndexOf("-") + 1, line.IndexOf(" ") - 2);
+                var character = line.Substring(line.IndexOf(" ") + 1, line.IndexOf(":"));
+                var password = line.Substring(line.IndexOf(":") + 2);
+
+                var policy = new PasswordPolicy(int.Parse(min), int.Parse(max), character[0], password);
+                
+                if (policy.SatisfiesLengthConstraint())
+                {
+                    correctPasswordsPart1++;
+                }
+
+                if (policy.SatisfiesPositionConstraint())
+                {
+                    correctPasswordsPart2++;
+                }
+            }
+
+            file.Close();
+            Console.WriteLine($"Correct passwords 1: {correctPasswordsPart1}");
+            Console.WriteLine($"Correct passwords 2: {correctPasswordsPart2}");
+        }
+    }
+}
