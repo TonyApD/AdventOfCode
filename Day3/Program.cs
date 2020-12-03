e6a26fb01ffe97a403b3f1e81e7f4e63b3983d30
Tony Lopar <tonylopar@hotmail.com>
2020-12-03 21:39:53 +0100

    Added day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var map = new List<string>();
+
+            var path = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, args[0]));
+            var file = new StringReader(path);
+
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                map.Add(line);
+            }
+            file.Close();
+
+            ExecutePart1(map);
+            ExecutePart2(map);
+        }
+
+        private static void ExecutePart1(IEnumerable<string> map)
+        {
+            var slope = new Slope(3, 1);
+            var xPos = 0;
+            for (int i = 0; i < map.Count(); i += slope.StepsDown)
+            {
+                var current = map.ElementAt(i);
+                var currentChar = current.ToCharArray()[xPos % current.Length];
+
+                if (currentChar == '#')
+                {
+                    slope.AddTree();
+                }
+
+                xPos += slope.StepsRight;
+            }
+            Console.WriteLine($"Part 1 - Number of trees: {slope.NumberOfTrees}");
+        }
+
+        private static void ExecutePart2(IEnumerable<string> map)
+        {
+            var slopes = new List<Slope>
+            {
+                new Slope(1, 1),
+                new Slope(3, 1),
+                new Slope(5, 1),
+                new Slope(7, 1),
+                new Slope(1, 2)
+            };
+
+            var product = 1L;
+            foreach (var slope in slopes)
+            {
+                var xPos = 0;
+                for (int i = 0; i < map.Count(); i += slope.StepsDown)
+                {
+                    var current = map.ElementAt(i);
+                    var currentChar = current.ToCharArray()[xPos % current.Length];
+
+                    if (currentChar == '#')
+                    {
+                        slope.AddTree();
+                    }
+                    xPos += slope.StepsRight;
+                }
+                Console.WriteLine($"Part2 - Number of trees: {slope.NumberOfTrees} {product}");
+                product *= slope.NumberOfTrees;
+            }
+            Console.WriteLine($"Part 2 - Number of trees product: {product}");
+        }
+    }
+}
---
+++ Day3/Slope.cs
@@ -0,0 +1,23 @@
+﻿namespace Day3
+{
+    class Slope
+    {
+        public int StepsRight { get; }
+
+        public int StepsDown { get; }
+
+        public int NumberOfTrees { get; private set; }
+
+        public Slope(int right, int down)
+        {
+            StepsRight = right;
+            StepsDown = down;
+            NumberOfTrees = 0;
+        }
+
+        public void AddTree()
+        {
+            NumberOfTrees++;
+        }
+    }
+}
