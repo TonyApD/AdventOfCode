d7d17fe0a9b8ba1e27baf89c947e172523e6e828
Tony Lopar <tonylopar@hotmail.com>
2020-12-21 19:40:45 +0100

    Added base for day 4

---
+++ Day4/Passport.cs
@@ -0,0 +1,26 @@
+﻿namespace Day4
+{
+    class Passport
+    {
+        public int? BirthYear { get; }
+
+        public int? IssueYear { get; }
+
+        public int? ExpirationYear { get; } 
+
+        public int? Height { get; }
+
+        public string? HairColor { get; }
+
+        public string? EyeColor { get; }
+
+        public long? PassportId { get; }
+
+        public long? CountryId { get; }
+
+        public Passport(string input)
+        {
+            
+        }
+    }
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var passports = new List<Passport>();
+
+            var path = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, args[0]));
+            var file = new StringReader(path);
+
+            var passport = "";
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line != "")
+                {
+                    passport += " " + line;
+                } else
+                {
+                    passports.Add(new Passport(passport));
+                    passport = "";
+                }
+            }
+            file.Close();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
